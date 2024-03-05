93ace928dd2be180818b6c085453670e27777b0e
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-03-05 23:13:38 +0500

    add commit upload

--- app.js
+++ app.js
@@ -219,29 +219,29 @@
 //    document.write("Incorret")
 // }
 
-var percent = +prompt("Enter your percent")
-if (percent >= 80 && percent <= 100){
-   alert("A+")
-}
-else if(percent >= 70 && percent <= 80){
-   alert("A")
-}
-else if(percent >= 60 && percent <= 70)
-alert("B")
+// var percent = +prompt("Enter your percent")
+// if (percent >= 80 && percent <= 100){
+//    alert("A+")
+// }
+// else if(percent >= 70 && percent <= 80){
+//    alert("A")
+// }
+// else if(percent >= 60 && percent <= 70)
+// alert("B")
 
-else if(percent >= 50 && percent <= 60){
-   alert("C")
-}
+// else if(percent >= 50 && percent <= 60){
+//    alert("C")
+// }
 
-else if(percent >= 40 && percent <= 50){
-   alert("D")
-}
-else if(percent >= 33 && percent <= 40){
-   alert("E")
-}
-else if(percent >= 0 && percent <= 33){
-   alert("F")
-}
-else{
-   alert("You didn't write correct percentage")
-}
+// else if(percent >= 40 && percent <= 50){
+//    alert("D")
+// }
+// else if(percent >= 33 && percent <= 40){
+//    alert("E")
+// }
+// else if(percent >= 0 && percent <= 33){
+//    alert("F")
+// }
+// else{
+//    alert("You didn't write correct percentage")
+// }
